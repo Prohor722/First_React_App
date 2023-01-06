99801901a2a1e2800d4615972ab42da59593b012
Prohor722 <prohor.b722@gmail.com>
2023-01-06 23:58:25 +0600

    basics

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style="color:red;">Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <SafeAreaView style={styles.container}>
+      {/* <Text style="color:red;">Open up App.js to start working on your app!</Text> */}
+      {/* <StatusBar style="auto" /> */}
       <View style={styles.colors}>
         <View style={{height:100, width: 100, backgroundColor:'red' }}></View>
-        <View style={{height:100, width: 100, backgroundColor:'blue' }}></View>
+        <View
+          style={{height:100, width: 100, backgroundColor:'blue', color: 'white' }}>
+          <Text onPress ={()=>{console.log("Hello !!");}} style={{color:'white', textAlign: 'center', marginTop:40}}>Hello Friends</Text>
+        </View>
         <View style={{height:100, width: 100, backgroundColor:'green' }}></View>
       </View>
-    </View>
+        <Button color="orange" title="Press Here" onPress={()=>console.log('Button Pressed')}/>
+    </SafeAreaView>
   );
 }
 
@@ -21,9 +25,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginBottom: 100,
   },
   colors: {
     flex:1,
     flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
   }
 });
